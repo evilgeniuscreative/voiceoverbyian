bd075d7a3de57ce29f9455e25baf1d43b08b9321
Evilgenuiscreative <mydesignguy@gmail.com>
2024-11-14 16:16:14 -0500

    With nice control button

--- src/app/components/AudioVisualzation.tsx
+++ src/app/components/AudioVisualzation.tsx
@@ -1,5 +1,9 @@
 "use client"
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef, useMemo, useCallback, } from 'react';
+import { createRoot } from 'react-dom/client'
+import Timeline from 'wavesurfer.js/dist/plugins/timeline.esm.js'
+import { useWavesurfer } from '@wavesurfer/react'
+
 import WaveSurfer from 'wavesurfer.js';
 
 // Define the interface for the props
@@ -8,57 +12,53 @@ interface AudioVisualzationProps {
 }
 
 const AudioVisualzation: React.FC<AudioVisualzationProps> = ({ audioSrc }) => {
-    const waveformRef = useRef(null);
-    const wavesurferRef = useRef<WaveSurfer | null>(null);
-
-    useEffect(() => {
-        if (waveformRef.current) {
-          const wavesurfer = WaveSurfer.create({
-            container: waveformRef.current,
-            dragToSeek: true,
-            waveColor: '#3b588a',
-            progressColor: '#383351',
-          });
+    let audioSource:string = audioSrc;
+    if (audioSrc === "commercial") {
+        audioSource = "../audio/Ian_Kleinfeld_Commercial_Demo.mp3";
+      } else if (audioSrc ==="character"){
+        audioSource =  "../audio/Ian_Kleinfeld_Character_Game_Animation_Demo.mp3"
+      }
     
-          wavesurfer.load(audioSource);
-          wavesurfer.load(audioSrc);
-        }
-      }, [audioSrc]);
+    const containerRef = useRef(null)
+    const [urlIndex, setUrlIndex] = useState(0)
+    const formatTime = (seconds: number) => [seconds / 60, seconds % 60].map((v) => `0${Math.floor(v)}`.slice(-2)).join(':')
 
-  const handlePlay = () =>{
-    if (wavesurferRef.current) {
-        wavesurferRef.current.playPause();
-        console.log("play")
-      }
-  }
+    const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
+      container: containerRef,
+      height: 100,
+      waveColor: '#ffffff',
+      progressColor: 'rgb(74, 76, 210)',
+      barWidth:2,
+      url: audioSource,
+      plugins: useMemo(() => [Timeline.create()], []),
 
-  const handleStop = () =>{
-    if (wavesurferRef.current) {
-        wavesurferRef.current.stop();
-        console.log("stop")
-      }
-  }
+    })
+  
+    const onUrlChange = useCallback(() => {
+      setUrlIndex((index) => (index + 1) % audioSource.length)
+    }, [])
+  
+    const onPlayPause = useCallback(() => {
+      wavesurfer && wavesurfer.playPause()
+    }, [wavesurfer])
 
-  const handleDownload = () =>{
-    document.getElementById('download')
-  }
-
-  let audioSource = audioSrc;
-  if (audioSrc === "commercial") {
-    audioSource = "../audio/Ian_Kleinfeld_Commercial_Demo.mp3";
-  } else if (audioSrc ==="character"){
-    audioSource =  "../audio/Ian_Kleinfeld_Character_Game_Animation_Demo.mp3"
-  }
 
 
 
 
   return (
   <>
-  <section className='demo'>
-  <div id="waveform" ref={waveformRef}></div>
-  <button className="btn" onClick={handlePlay}>Play</button> <button className="btn" onClick={handleStop}>Stop</button> <a className="btn dl" href={audioSource} download>Download</a>
-  </section>
+        <section className='demo'>
+
+        <div ref={containerRef} />
+            <p className='timer'>Current time: {formatTime(currentTime)}</p>
+            <div className="btn-control">
+            <button className={isPlaying ? 'btn-pause' : 'btn'} onClick={onPlayPause} style={{ minWidth: '5em' }}>
+                {isPlaying ? 'Pause' : 'Play'}
+            </button>
+            <a className='btn dl' download href={audioSource} >Download</a> 
+            </div>
+        </section>
   </>
   )
 };
