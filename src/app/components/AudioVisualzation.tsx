4bf7d3b50f76ba2f6dabe8920b8cff2d4f7cc16a
Evilgenuiscreative <mydesignguy@gmail.com>
2024-11-14 15:47:16 -0500

    Added waveform script and is working on a basic level

--- src/app/components/AudioVisualzation.tsx
+++ src/app/components/AudioVisualzation.tsx
@@ -8,29 +8,59 @@ interface AudioVisualzationProps {
 }
 
 const AudioVisualzation: React.FC<AudioVisualzationProps> = ({ audioSrc }) => {
+    const waveformRef = useRef(null);
+    const wavesurferRef = useRef<WaveSurfer | null>(null);
+
+    useEffect(() => {
+        if (waveformRef.current) {
+          const wavesurfer = WaveSurfer.create({
+            container: waveformRef.current,
+            dragToSeek: true,
+            waveColor: '#3b588a',
+            progressColor: '#383351',
+          });
+    
+          wavesurfer.load(audioSource);
+          wavesurfer.load(audioSrc);
+        }
+      }, [audioSrc]);
+
+  const handlePlay = () =>{
+    if (wavesurferRef.current) {
+        wavesurferRef.current.playPause();
+        console.log("play")
+      }
+  }
+
+  const handleStop = () =>{
+    if (wavesurferRef.current) {
+        wavesurferRef.current.stop();
+        console.log("stop")
+      }
+  }
+
+  const handleDownload = () =>{
+    document.getElementById('download')
+  }
 
   let audioSource = audioSrc;
   if (audioSrc === "commercial") {
-    audioSource = "/audio/Ian_Kleinfeld_Commercial_Demo.mp3";
+    audioSource = "../audio/Ian_Kleinfeld_Commercial_Demo.mp3";
   } else if (audioSrc ==="character"){
-    audioSource =  "/audio/Ian_Kleinfeld_Character_Game_Animation_Demo.mp3"
+    audioSource =  "../audio/Ian_Kleinfeld_Character_Game_Animation_Demo.mp3"
   }
-  const waveformRef = useRef(null);
 
-  useEffect(() => {
-    if (waveformRef.current) {
-      const wavesurfer = WaveSurfer.create({
-        container: waveformRef.current,
-        waveColor: '#4F4A85',
-        progressColor: '#383351',
-      });
 
-      wavesurfer.load(audioSource);
-      wavesurfer.load(audioSrc);
-    }
-  }, [audioSrc]);
 
-  return <div id="waveform" ref={waveformRef}></div>;
+
+  return (
+  <>
+  <section className='demo'>
+  <div id="waveform" ref={waveformRef}></div>
+  <button className="btn" onClick={handlePlay}>Play</button> <button className="btn" onClick={handleStop}>Stop</button> <a className="btn dl" href={audioSource} download>Download</a>
+  </section>
+  </>
+  )
 };
 
 export default AudioVisualzation;
